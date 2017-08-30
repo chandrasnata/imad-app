9ebda851dcb3d47b9fa91424e1f8390cca75256e
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-08-30 10:44:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-One', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-Two', function (req, res) {
