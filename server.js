bb17ae0ee597a70a534bb807b5be24feb0e0f1b0
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-09-01 11:41:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone = {
+    title:'Article One 1 CHANDRASNATA DASH',
+    heading:'Article One',
+    date:'Aug 29,2017',
+    content:` 
+    <p>
+        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+    </p>
+    <p>
+        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+    </p>
+    <p>
+        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+    </p>`
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
