c3ac0ef066b77eb1449841d3333718cbf71ca20b
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-09-02 09:38:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleName==article-One
     //articles[articleName] == {}content object for articleone
-    var articleName = req.parames.articleName
+    var articleName = req.params.articleName
   res.send(createTemplate(article[articleName]));
 });
 
