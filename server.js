c89b0e88ffedac907decb27bc9277657e36a09fb
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-09-12 10:46:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res) {
+    counter = counter+1;
+    res.send(counter.tostring());
+});
 app.get('/:articleName', function (req, res) {
     //articleName == article-One
     //articles[articleName] == {}content object for articleone
