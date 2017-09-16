293f943c6dad97ffa6e22ca9d9e9c0bc2eb77e45
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-09-16 20:08:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ button.onClick = function () {
     request.open('GET','http://chandrasnatadash.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = name Input.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick = function () {
+    
+    //Make a request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length; i++){
+        list += '<li>' +names[i]+ '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
