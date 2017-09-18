8d2e96c723e044e3c082e11c57858f28866d4e3c
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-09-19 00:38:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onClick = function () {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit-btn');
 submit.onclick = function () {
     
@@ -53,6 +52,8 @@ submit.onclick = function () {
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://chandrasnatadash.imad.hasura-app.io/submit-name?name='+name, true);
     request.send(null);
 };
