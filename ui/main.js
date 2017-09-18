8ccd54bdac436094c511dcfc0a306833ab924770
chandrasnata <31014903+chandrasnata@users.noreply.github.com>
2017-09-19 00:16:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,14 +30,32 @@ var name = name Input.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function () {
     
+     //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                //Capture a list of names and render it as a list
+                var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for(var i=0;i<names.length; i++){
+                list += '<li>' +names[i]+ '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        //Not done yet
+    };
+    
+    //Make the request
+    request.open('GET','http://chandrasnatadash.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
     //Make a request to the server and send the name
     
-    //Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length; i++){
-        list += '<li>' +names[i]+ '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+   
 };
